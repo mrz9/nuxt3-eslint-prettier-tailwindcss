36d5185e3ab6f03bb88b1da8637c4bb55ca8aa3a
mrz <1711231134@qq.com>
2023-07-19 21:29:08 +0700

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="p-5">
+        <div class="text-red-500">红色字体</div>
+        <div class="mt-2 c">
+            <p class="s bg-primary">scon</p>
+        </div>
+    </div>
+</template>
+<style>
+.app {
+    color: red;
+}
+.s {
+    display: flex;
+    @apply text-lime-700;
+}
+</style>
